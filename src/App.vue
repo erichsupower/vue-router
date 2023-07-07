4dc525dfcbd92b2498ed1401b2628f73fcdc7b9c
erichsupower <erichsupower@gmail.com>
2023-07-07 23:22:28 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav>
+    <router-link to="/">页面1</router-link>
+    <router-link to="/2">页面2</router-link>
+    <router-link to="/3">页面3</router-link>
+  </nav>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, 'PingFang SC', 'Microsoft Yahei', sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(
+    #212943 0.6000000000000001px,
+    #0f141c 0.6000000000000001px
+  );
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+nav {
+  padding: 24px 0;
+}
+
+nav a {
+  font-size: 18px;
+  color: white;
+  margin-right: 24px;
+}
+</style>
